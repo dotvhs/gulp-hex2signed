8fa594b9a1e897c07b92e5c5711545236b9209c9
dotvhs <voythas@gmail.com>
2019-09-14 03:09:23 +0200

    Initial push

---
+++ index.js
@@ -0,0 +1,48 @@
+var through = require("through2");
+var gutil = require("gulp-util");
+var PluginError = gutil.PluginError;
+
+var PLUGIN_NAME = "gulp-hex2signed";
+
+function hexToInt(hex) {
+	if (hex.length % 2 != 0) {
+		hex = "0" + hex;
+	}
+	var num = parseInt(hex, 16);
+	var maxVal = Math.pow(2, (hex.length / 2) * 8);
+	if (num > maxVal / 2 - 1) {
+		num = num - maxVal;
+	}
+	return num;
+}
+
+var hexint = function(css) {
+	return css.replace(/#[0-9a-fA-f]{3,8}/g, function(match) {
+		var newMatch = match + "; android: " + hexToInt(match);
+		return newMatch;
+	});
+};
+
+function gulpPrefixer() {
+	var stream = through.obj(function(file, enc, cb) {
+		if (file.isStream()) {
+			this.emit(
+				"error",
+				new PluginError(PLUGIN_NAME, "Streams are not supported!")
+			);
+			return cb();
+		}
+
+		if (file.isBuffer()) {
+			file.contents = new Buffer(hexint(file.contents.toString()));
+		}
+
+		this.push(file);
+
+		cb();
+	});
+
+	return stream;
+}
+
+module.exports = gulpPrefixer;
